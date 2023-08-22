cc14e666191862e525dc3a96a2b78df75ac0035e
Artur Miłkowski <artur-milkowski@tlen.pl>
2023-08-22 18:32:00 +0200

    Add the admin category views

---
+++ resources/views/backend/admin/product/category/create.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dodawanie</h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="{{ route('backend.admins.products.categories.store') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="slug">
+                        <div>
+                            <label for="name">Kategoria</label>
+                            <input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="Pole obowiązkowe" autofocus>
+                            <x-input-error :messages="$errors->get('name')" />
+                        </div>
+                        <a href="{{ route('backend.admins.products.categories.index') }}">Powrót</a> |
+                        <x-primary-button class="mt-4">Dodaj</x-primary-button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/backend/admin/product/category/index.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Kategorie</h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p>
+                        <a href="{{ route('backend.admins.products.categories.create') }}">Dodaj</a>
+                    </p>
+                    <table class="w-full">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nazwa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+@forelse ($collection as $item)
+                            <tr>                                
+                                <x-table-data>{{ $item->id }}</x-table-data>
+                                <x-table-data>
+                                    <a href="{{ route('backend.admins.products.categories.show', $item) }}">
+                                        {{ $item->name }}
+                                    </a>
+                                </x-table-data>
+                            </tr>
+@empty
+                            <tr>
+                                <td>Brak danych</td>
+                            </tr>
+@endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
